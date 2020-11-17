b238ad915d5fbd805a6d349b9d28d673b9ae9ac4
Iwanezuk <iwanezuk@hotmail.com>
2020-11-17 17:24:27 -0300

    Create conn_produtos.php

---
+++ Connections/conn_produtos.php
@@ -0,0 +1,21 @@
+<?php
+// Definição de variáveis para conexão
+$hostname_conn  = "localhost";
+$database_conn  = "iwane047_ti11";
+$username_conn  = "iwane047_ti11";
+$password_conn  = "senacti11";
+$charset_conn   = "utf8";
+
+// Definindo parâmetros da conexão
+$conn_produtos  = new mysqli($hostname_conn,$username_conn,$password_conn,$database_conn);
+
+// Definindo o conjunto de caracteres da conexão
+mysqli_set_charset($conn_produtos,$charset_conn);
+
+// Verificando possíveis erros na conexão
+if($conn_produtos->connect_error){
+    echo "Erro: ".$conn_produtos->connect_error;
+};
+// Não deixar espaços vazios depois do fechamento do PHP
+// Pode causar erros do tipo "header"
+?>
