399fe001d801c0f2ff18a47868cf1aa60ccfb272
Iwanezuk <iwanezuk@hotmail.com>
2020-11-23 17:35:17 -0300

    Update produtos_lista.php

--- admin/produtos_lista.php
+++ admin/produtos_lista.php
@@ -20,6 +20,11 @@ $totalRows  = ($lista)->num_rows;
 <title>Produtos - Lista</title>
 <meta charset="utf-8">
 <!-- Depois vamos inserir aqui o Bootstrap -->
+<!-- Link arquivos Bootstrap css -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="../css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 <!-- body>main>h1 -->
 <body>
@@ -36,7 +41,11 @@ $totalRows  = ($lista)->num_rows;
                 <th>RESUMO</th>
                 <th>VALOR</th>
                 <th>IMAGEM</th>
-                <th>ADICIONAR</th>
+                <th>
+                    <a href="produtos_insere.php" target="_self">
+                        ADICIONAR
+                    </a>
+                </th>
             </tr>
         </thead>
         <!-- tbody>tr>td*8 -->    
@@ -54,14 +63,45 @@ $totalRows  = ($lista)->num_rows;
                 <td>
                     <img src="../imagens/<?php echo $row['imagem_produto']; ?>" alt="" width="100px">
                 </td>
-                <td>ALTERAR|EXCLUIR</td>
+                <td>ALTERAR|
+                    <button class="btn btn-danger btn-block btn-xs delete" role="button" data-nome="<?php echo $row['descri_produto']; ?>" data-id="<?php echo $row['id_produto']; ?>">
+                        <span class="hidden-xs">EXCLUIR<br></span>
+                        <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+                    </button>
+                </td>
             </tr>
             <?php } while ($row = $lista->fetch_assoc()); ?>
             <!-- Fecha a estrutura de repetição -->
         </tbody>
     </table>
-    
 </main>
+<!-- MODAL -->
+<div id="myModal" class="modal fade" role="dialog">
+   <div class="modal-dialog">
+      <div class="modal-content">
+         <div class="modal-header">
+             <button type="button" class="close" data-dismiss="modal">&times;</button>
+             <h4 class="modal-title text-danger">ATENÇÃO!</h4>
+         </div><!-- fecha modal-header -->
+         <div class="modal-body">
+            Deseja mesmo EXCLUIR o item?
+            <h3><span class="text-danger nome"></span></h3>
+         </div><!-- fecha modal-body -->
+         <div class="modal-footer">
+            <a href="#" type="button" class="btn btn-danger delete-yes">
+                Confirmar
+            </a>
+            <button class="btn btn-success" data-dismiss="modal">
+                Cancelar
+            </button>             
+         </div><!-- fecha modal-footer --> 
+      </div><!-- fecha modal-content -->    
+   </div><!-- fecha modal-dialog -->    
+</div>
+<!-- fecha MODAL -->
+<!-- Link arquivos Bootstrap js -->        
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
 </body>
 </html>
 <?php mysqli_free_result($lista); ?>
