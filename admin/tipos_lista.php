44ce8bf3a947c932044f32402983495e47f062a2
Iwanezuk <iwanezuk@hotmail.com>
2020-11-25 15:03:31 -0300

    Update tipos_lista.php

--- admin/tipos_lista.php
+++ admin/tipos_lista.php
@@ -18,7 +18,12 @@ $totalRows  =   ($lista)->num_rows;
 <head>
 <title>Tipos - Lista</title>
 <meta charset="utf-8">
-<!-- Depois vamos inserir aqui o Bootstrap -->    
+<!-- Depois vamos inserir aqui o Bootstrap -->
+<!-- Link arquivos Bootstrap css -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    
 </head>
 <!-- body>main>h1 -->
 <body>
@@ -47,13 +52,60 @@ $totalRows  =   ($lista)->num_rows;
                <td><?php echo $row['id_tipo']; ?></td>
                <td><?php echo $row['sigla_tipo']; ?></td>
                <td><?php echo $row['rotulo_tipo']; ?></td>
-               <td>ALTERAR|EXCLUIR</td>
+               <td>ALTERAR|
+                    <button class="btn btn-danger btn-block btn-xs delete" role="button" data-nome="<?php echo $row['rotulo_tipo']; ?>" data-id="<?php echo $row['id_tipo']; ?>">
+                        <span class="hidden-xs">EXCLUIR<br></span>
+                        <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+                    </button>
+                </td>
            </tr>
            <?php } while ($row = $lista->fetch_assoc()); ?>
            <!-- Fecha estrutura de repetição -->
        </tbody>    
     </table>
 </main>
+<!-- MODAL -->
+<div id="myModal" class="modal fade" role="dialog">
+   <div class="modal-dialog">
+      <div class="modal-content">
+         <div class="modal-header">
+             <button type="button" class="close" data-dismiss="modal">&times;</button>
+             <h4 class="modal-title text-danger">ATENÇÃO!</h4>
+         </div><!-- fecha modal-header -->
+         <div class="modal-body">
+            Deseja mesmo EXCLUIR o item?
+            <h3><span class="text-danger nome"></span></h3>
+         </div><!-- fecha modal-body -->
+         <div class="modal-footer">
+            <a href="#" type="button" class="btn btn-danger delete-yes">
+                Confirmar
+            </a>
+            <button class="btn btn-success" data-dismiss="modal">
+                Cancelar
+            </button>             
+         </div><!-- fecha modal-footer --> 
+      </div><!-- fecha modal-content -->    
+   </div><!-- fecha modal-dialog -->    
+</div>
+<!-- fecha MODAL -->
+<!-- Link arquivos Bootstrap js -->        
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+
+<!-- Script para o Modal -->
+<script type="text/javascript">
+    $('.delete').on('click',function(){
+        var nome    =  $(this).data('nome');
+        // buscar o valor do atributo data-nome
+        var id      =  $(this).data('id');
+        // buscar o valor do atributo data-id
+        $('span.nome').text(nome);
+        // Inserir o nome do item na confirmação do Modal
+        $('a.delete-yes').attr('href','tipos_exclui.php?id_tipo='+id);
+        // Enviar o id através do link do botão confirmar
+        $('#myModal').modal('show'); //Abre o modal
+    });
+</script>
 </body>
 </html>
 <?php mysqli_free_result($lista); ?>
