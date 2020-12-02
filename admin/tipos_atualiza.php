10a123683777c03a219afcc38915fd7805b8d55b
Iwanezuk <iwanezuk@hotmail.com>
2020-12-02 13:47:12 -0300

    Create tipos_atualiza.php

---
+++ admin/tipos_atualiza.php
@@ -0,0 +1,118 @@
+<?php
+// Incluir o arquivo e fazer a conexão
+include("../Connections/conn_produtos.php");
+
+// Variáveis Globais
+$tabela         =   "tbtipos";
+$campo_filtro   =   "id_tipo";
+
+if($_POST){
+    // Definindo o USE do banco de dados
+    mysqli_select_db($conn_produtos,$database_conn);
+        
+    // Receber os dados do formulário
+    // Organize os campos na mesma ordem
+    $sigla_tipo    =   $_POST['sigla_tipo'];
+    $rotulo_tipo   =   $_POST['rotulo_tipo'];
+    
+    // Campo para filtrar o registro (WHERE)
+    $filtro_update  =   $_POST['id_tipo'];
+    
+    // Consulta SQL para atualização dos dados
+    $updateSQL  =   "UPDATE ".$tabela."
+                        SET sigla_tipo  = '".$sigla_tipo."',
+                            rotulo_tipo = '".$rotulo_tipo."'
+                        WHERE ".$campo_filtro."='".$filtro_update."'
+                    ";
+    $resultado  =   $conn_produtos->query($updateSQL);
+    
+    // Após a ação a página será redirecionada
+    $destino    =   "tipos_lista.php";
+    if(mysqli_insert_id($conn_produtos)){
+        header("Location: $destino");
+    }else{
+        header("Location: $destino");
+    };    
+};
+
+// Consulta para trazer e filtrar os dados
+// Definindo o USE do banco de dados
+mysqli_select_db($conn_produtos,$database_conn);
+$filtro_select  =   $_GET['id_tipo'];
+$consulta       =   "SELECT *
+                    FROM ".$tabela."
+                    WHERE ".$campo_filtro."=".$filtro_select."
+                    ";
+$lista          =   $conn_produtos->query($consulta);
+$row            =   $lista->fetch_assoc();
+$totalRows      =   ($lista)->num_rows;
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<title>Tipos - Atualiza</title>
+<meta charset="utf-8">
+<!-- Link arquivos Bootstrap css -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="../css/bootstrap.min.css" rel="stylesheet">
+<!-- Link para CSS específico -->
+<link rel="stylesheet" href="../css/meu_estilo.css" type="text/css">
+</head>
+<body class="fundofixo">
+<main class="container">
+<div class="row">
+<div class="col-xs-12 col-sm-offset-3 col-sm-6 col-md-offset-4 col-md-4"><!-- dimensionamento -->
+<h2 class="breadcrumb text-warning">
+   <a href="tipos_lista.php">
+       <button class="btn btn-warning" type="button">
+           <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+       </button>
+   </a>
+    Atualizando tipos
+</h2>
+<div class="thumbnail">
+    <div class="alert alert-warning">
+       <form action="tipos_atualiza.php" name="form_atualiza_tipo" id="form_atualiza_tipo" method="post" enctype="multipart/form-data">
+        
+        <!-- Inserir o campo id_tipo oculto para uso em filtros -->
+        <input type="hidden" id="id_tipo" name="id_tipo" value="<?php echo $row['id_tipo']; ?>">
+          
+          <!-- input rotulo_tipo -->
+          <label for="rotulo_tipo">Rótulo</label>
+          <div class="input-group">
+             <span class="input-group-addon">
+                 <span class="glyphicon glyphicon-apple" aria-hidden="true"></span>
+             </span>
+             <input type="text" name="rotulo_tipo" id="rotulo_tipo" maxlength="15" placeholder="Digite o tipo do produto." required autofocus class="form-control" value="<?php echo $row['rotulo_tipo']; ?>">
+          </div><!-- fecha input-group -->
+          <br>
+          <!-- fecha input rotulo_tipo -->
+          
+          <!-- input sigla_tipo -->
+          <label for="sigla_tipo">Sigla</label>
+          <div class="input-group">
+             <span class="input-group-addon">
+                 <span class="glyphicon glyphicon-th-list" aria-hidden="true"></span>
+             </span>
+             <input type="text" name="sigla_tipo" id="sigla_tipo" maxlength="3" placeholder="Digite a sigla do tipo." required class="form-control" value="<?php echo $row['sigla_tipo']; ?>">
+          </div><!-- fecha input-group -->
+          <br>
+          <!-- fecha input rotulo_tipo -->
+          
+          <!-- btn enviar -->
+          <input type="submit" value="Atualizar" role="button" name="enviar" id="enviar" class="btn btn-block btn-warning">
+          
+       </form>
+    </div><!-- fecha alert -->
+</div><!-- fecha thumbnail -->
+</div><!-- fecha dimensionamento -->
+</div><!-- fecha row -->  
+</main>
+
+<!-- Link arquivos Bootstrap js -->        
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+</body>
+</html>
+<?php mysqli_free_result($lista); ?>
