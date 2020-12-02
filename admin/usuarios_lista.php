2e8b696f7c1c81e9e20bf6beb9ac943a37280e37
Iwanezuk <iwanezuk@hotmail.com>
2020-12-02 16:07:23 -0300

    Update usuarios_lista.php

--- admin/usuarios_lista.php
+++ admin/usuarios_lista.php
@@ -21,21 +21,23 @@ $totalRows  =   ($lista)->num_rows;
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="../css/bootstrap.min.css" rel="stylesheet">
-
+<link rel="stylesheet" href="../css/meu_estilo.css" type="text/css">
 </head>
-<body>
+<body class="fundofixo">
 <?php include "menu_adm.php"; ?>
-<main>
-    <h1>Lista de Usuários</h1>
-    <table border="1">
+<main class="container">
+    <h1 class="breadcrumb alert-info">Lista de Usuários</h1>
+    <div class="col-xs-12 col-sm-offset-3 col-sm-6 col-md-offset-3 col-md-6">
+    <table class="table table-condensed table-hover tbopacidade">
         <thead><!-- cabeçalho da tabela -->
             <tr>
-                <th>ID</th><!-- cabeça da coluna -->
+                <th class="hidden">ID</th><!-- cabeça da coluna -->
                 <th>LOGIN</th>
                 <th>NÍVEL</th>
                 <th>
-                    <a href="usuarios_insere.php">
-                        ADICIONAR
+                    <a href="usuarios_insere.php" target="_self" class="btn btn-block btn-primary btn-xs" role="button">
+                        <span class="hidden-xs">ADICIONAR<br></span>
+                        <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
                     </a>
                 </th>
             </tr>
@@ -44,10 +46,23 @@ $totalRows  =   ($lista)->num_rows;
            <!-- estrutura de repetição -->
            <?php do { ?>
             <tr>
-                <td><?php echo $row['id_usuario']; ?></td>
+                <td class="hidden"><?php echo $row['id_usuario']; ?></td>
                 <td><?php echo $row['login_usuario']; ?></td>
-                <td><?php echo $row['nivel_usuario']; ?></td>
-                <td>ALTERAR
+                <td>
+                    <?php
+                        if($row['nivel_usuario']=='com'){
+                            echo ("<span class='glyphicon glyphicon-user text-info' aria-hidden='true'></span>");
+                        }else if($row['nivel_usuario']=='sup'){
+                            echo ("<span class='glyphicon glyphicon-sunglasses text-default' aria-hidden='true'></span>");
+                        };
+                    ?>
+                    <?php echo $row['nivel_usuario']; ?>
+                </td>
+                <td>
+                   <a href="" target="_self" class="btn btn-block btn-warning btn-xs" role="button">
+                        <span class="hidden-xs">ALTERAR<br></span>
+                        <span class="glyphicon glyphicon-refresh" aria-hidden="true"></span>
+                    </a>
                     <button class="btn btn-danger btn-block btn-xs delete" role="button" data-nome="<?php echo $row['login_usuario']; ?>" data-id="<?php echo $row['id_usuario']; ?>">
                         <span class="hidden-xs">EXCLUIR<br></span>
                         <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
@@ -58,6 +73,7 @@ $totalRows  =   ($lista)->num_rows;
             <!-- fecha estrutura de repetição -->
         </tbody>
     </table>
+    </div><!-- fecha dimensionamento -->
 </main>
 <!-- MODAL -->
 <div id="myModal" class="modal fade" role="dialog">
