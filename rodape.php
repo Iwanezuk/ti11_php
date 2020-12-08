bf5806e8179c09fc881fd3690d6807b51cda4a66
Iwanezuk <iwanezuk@hotmail.com>
2020-12-08 14:16:39 -0300

    Update rodape.php

--- rodape.php
+++ rodape.php
@@ -10,7 +10,38 @@
 <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
 <link href="css/meu_estilo.css" rel="stylesheet" type="text/css">
 </head>
-<body>
+<body class="fundofixo">
+<!-- Abre o painel Rodapé -->
+<div class="row panel-footer" style="background-color:rgba(255,255,255,0.8);">
+
+<!-- Abre ÁREA DE LOCALIZAÇÃO -->
+<div class="col-sm-6 col-md-4">
+    <div class="panel-footer" style="background: none;">
+        <img src="imagens/logochurrascopequeno.png" alt="">
+        <br>
+        <i>O melhor churrasco da região!</i>
+        <address>
+            <i>Rua Dom Joaquim, 495 - Centro - Itapetininga - SP - CEP 18200-000</i>
+            <br>
+            <span class="glyphicon glyphicon-phone-alt"></span>
+            &nbsp;Fone: (15) 3511 1200
+            <br>
+            <span class="glyphicon glyphicon-envelope"></span>
+            &nbsp;Email:
+            <a href="mailto:contato@chuletaquente.com.br?subject=Contato&cc=seuemail@hotmail.com">
+                contato@chuletaquente.com.br
+            </a> 
+        </address>
+        <div class="embed-responsive embed-responsive-16by9">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.3527067162086!2d-48.054867884475236!3d-23.591680368598958!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c5cc93b46246ed%3A0x6ec0870ce87bb6fd!2sSenac%20Itapetininga!5e0!3m2!1spt-BR!2sbr!4v1568163169642!5m2!1spt-BR!2sbr" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+        </div><!-- fecha div embed-responsive -->
+    </div><!-- fecha panel-footer -->
+</div><!-- fecha dimensionamento -->
+<!-- Fecha ÁREA DE LOCALIZAÇÃO -->
+
+
+</div><!-- fecha panel-footer -->
+<!-- Fecha o painel Rodapé -->
 
 <!-- Link arquivos Bootstrap js -->        
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
