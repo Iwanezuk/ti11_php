3868f1cdb5e20b78342cfd839f7517b23ff895c1
Iwanezuk <iwanezuk@hotmail.com>
2020-12-08 13:54:01 -0300

    Create rodape.php

---
+++ rodape.php
@@ -0,0 +1,19 @@
+<!-- Salvar como: rodape.php -->
+<!doctype html>
+<html lang="pt-br">
+<head>
+<title>Chuleta Quente</title>
+<meta charset="utf-8">
+<!-- Link arquivos Bootstrap css -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="css/meu_estilo.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<!-- Link arquivos Bootstrap js -->        
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
